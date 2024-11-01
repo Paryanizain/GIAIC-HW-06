3e890bc315902977ab354d50adebdd016c39e86f
zain <paryanizain@gmail.com>
2024-11-01 21:12:34 +0500

    first commit

---
+++ class-06-hm/my-app/app/page.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import Footer from "./components/Footer";
+import Herosection from "./components/Herosection";
+
+export default function Home() {
+  return (
+    <div>
+  
+      <Herosection />
+      <div className="bg-slate-100 py-8 px-10 dark:bg-slate-700">
+        <div className="flex justify-between">
+        <h1 className="font-bold text-2xl text-black">Featured New Cars</h1>
+        <div className="text-blue-700 text-sm font-medium hover:text-red-700">View All New Cars</div>
+        </div>
+        <ul className="flex font-semibold py-6 space-x-10 text-black">
+          <li className="hover:text-red-700">Popular</li>
+          <li className="hover:text-red-700">Upcoming</li>
+          <li className="hover:text-red-700">Newly Launched</li>
+        </ul>
+        
+        <div className="flex space-x-6 text-center">
+        <ul className="w-1/4 bg-white">
+        <Link href="/Corolla-details"><li className=" hover:opacity-50 transition duration-300"><img src="/Corolla-X-Cars.jpg" alt="Toyota Corolla" /></li></Link>
+        <Link href="/Corolla-details"><li className="text-blue-900 font-semibold hover:text-red-700">Toyota Corolla</li></Link>
+        <Link href="/Corolla-details"><li className="text-green-500 font-semibold my-2 hover:text-red-700">PKR 59.7 - 75.5 lacs</li></Link>
+        </ul>
+
+        <ul className="w-1/4 bg-white">
+        <Link href="/City-details"><li className=" hover:opacity-50 transition duration-300"><img src="/Honda_City.jpg" alt="Honda City" /></li></Link>
+        <Link href="/City-details"><li className="text-blue-900 font-semibold hover:text-red-700">Honda City</li></Link>
+        <Link href="/City-details"><li className="text-green-500 font-semibold my-2 hover:text-red-700">PKR 45.5 - 58.5 lacs</li></Link>
+        </ul>
+
+        <ul className="w-1/4 bg-white">
+        <Link href="/Juke-details"><li className=" hover:opacity-50 transition duration-300"><img src="/Nissan_Juke01.png" alt="Nissan Juke" /></li></Link>
+        <Link href="/Juke-details"><li className="text-blue-900 font-semibold hover:text-red-700">Nissan Juke</li></Link>
+        <Link href="/Juke-details"><li className="text-green-500 font-semibold my-2 hover:text-red-700">PKR 52.5 - 65.5 lacs</li></Link>
+        </ul>
+
+        <ul className="w-1/4 bg-white">
+        <Link href="/Alto-details"><li className=" hover:opacity-50 transition duration-300"><img src="/Suzuki_Alto_2015.jpg" alt="Suzuki Alto" /></li></Link>
+        <Link href="/Alto-details"><li className="text-blue-900 font-semibold hover:text-red-700">Suzuki Alto</li></Link>
+        <Link href="/Alto-details"><li className="text-green-500 font-semibold my-2 hover:text-red-700">PKR 23.3 - 32.5 lacs</li></Link>
+        </ul>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
